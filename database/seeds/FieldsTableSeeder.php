5e1d907ec6d7d1a302b85a249267f509f002dc83
Netto Ramalho <netto.ramalho@gmail.com>
2018-09-18 21:10:34 -0300

    database seed for websites and fields

---
+++ database/seeds/FieldsTableSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class FieldsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('fields')->insert([
+            'model' => 'website',
+            'name' => 'name',
+            'label' => 'Name',
+            'type' => 'text'
+        ]);
+        DB::table('fields')->insert([
+            'model' => 'website',
+            'name' => 'domain',
+            'label' => 'Domain',
+            'type' => 'text'
+        ]);
+        DB::table('fields')->insert([
+            'model' => 'website',
+            'name' => 'created_at',
+            'label' => 'Created at',
+            'type' => 'dateTime'
+        ]);
+        DB::table('fields')->insert([
+            'model' => 'website',
+            'name' => 'updated_at',
+            'label' => 'Updated at',
+            'type' => 'dateTime'
+        ]);
+    }
+}
---
+++ database/seeds/WebsitesTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+
+class WebsitesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+        foreach (range(1,100) as $index) {
+            DB::table('websites')->insert([
+                'name' => $faker->domainWord,
+                'domain' => $faker->domainName,
+                'created_at' => $faker->dateTimeThisDecade()
+            ]);
+        }
+    }
+}
